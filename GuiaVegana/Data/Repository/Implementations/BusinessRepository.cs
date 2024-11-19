4cc615cae7620703509dd8f36a6e0c7a3f3ef5de
Olivia <olivialuciano42@gmail.com>
2024-11-19 18:37:44 -0300

    metodos del business

--- GuiaVegana/Data/Repository/Implementations/BusinessRepository.cs
+++ GuiaVegana/Data/Repository/Implementations/BusinessRepository.cs
@@ -1,5 +1,8 @@
 ﻿using AutoMapper;
 using GuiaVegana.Data.Repository.Interfaces;
+using GuiaVegana.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace GuiaVegana.Data.Repository.Implementations
 {
@@ -12,5 +15,101 @@ namespace GuiaVegana.Data.Repository.Implementations
             _context = context;
             _mapper = mapper;
         }
+        // Métodos GET
+
+        // 1. Traer todos los negocios
+        public IEnumerable<Business> GetAllBusinesses()
+        {
+            return _context.Businesses.Include(b => b.OpeningHours).Include(b => b.VeganOptions).ToList();
+        }
+
+        // 2. Traer un negocio por ID
+        public Business GetBusinessById(int id)
+        {
+            return _context.Businesses
+                .Include(b => b.OpeningHours)
+                .Include(b => b.VeganOptions)
+                .FirstOrDefault(b => b.Id == id);
+        }
+
+        // 3. Filtrar por calificaciones
+        public IEnumerable<Business> GetBusinessesByRatings(IEnumerable<Rating> ratings)
+        {
+            return _context.Businesses.Where(b => ratings.Contains(b.Rating)).ToList();
+        }
+
+        // 4. Filtrar por tipos de delivery
+        public IEnumerable<Business> GetBusinessesByDeliveries(IEnumerable<DeliveryType> deliveries)
+        {
+            return _context.Businesses.Where(b => deliveries.Contains(b.Delivery)).ToList();
+        }
+
+        // 5. Filtrar por tipos de negocio
+        public IEnumerable<Business> GetBusinessesByTypes(IEnumerable<BusinessType> businessTypes)
+        {
+            return _context.Businesses.Where(b => businessTypes.Contains(b.BusinessType)).ToList();
+        }
+
+        // 6. Filtrar por zonas
+        public IEnumerable<Business> GetBusinessesByZones(IEnumerable<Zone> zones)
+        {
+            return _context.Businesses.Where(b => zones.Contains(b.Zone)).ToList();
+        }
+
+        // 7. Traer negocios totalmente basados en plantas
+        public IEnumerable<Business> GetPlantBasedBusinesses()
+        {
+            return _context.Businesses.Where(b => b.AllPlantBased).ToList();
+        }
+
+        // 8. Traer negocios con opciones gluten-free
+        public IEnumerable<Business> GetGlutenFreeBusinesses()
+        {
+            return _context.Businesses.Where(b => b.GlutenFree).ToList();
+        }
+
+        // 9. Traer negocios abiertos en el momento actual
+        public IEnumerable<Business> GetBusinessesOpenAt(DateTime currentTime)
+        {
+            return _context.Businesses
+                .Include(b => b.OpeningHours)
+                .Where(b => b.OpeningHours.Any(oh =>
+                    oh.DayOfWeek == currentTime.DayOfWeek &&
+                    oh.OpenTime <= currentTime.TimeOfDay &&
+                    oh.CloseTime >= currentTime.TimeOfDay))
+                .ToList();
+        }
+
+        // Métodos POST
+
+        // 10. Agregar un nuevo negocio
+        public void AddBusiness(Business business)
+        {
+            _context.Businesses.Add(business);
+            _context.SaveChanges();
+        }
+
+        // Métodos PUT
+
+        // 11. Actualizar datos de un negocio existente
+        public void UpdateBusiness(Business business)
+        {
+            _context.Businesses.Update(business);
+            _context.SaveChanges();
+        }
+
+        // Métodos DELETE
+
+        // 12. Eliminar un negocio por ID
+        public void DeleteBusiness(int id)
+        {
+            var business = _context.Businesses.Find(id);
+            if (business != null)
+            {
+                _context.Businesses.Remove(business);
+                _context.SaveChanges();
+            }
+        }
     }
+}
 }
--- GuiaVegana/Data/Repository/Interfaces/IBusinessRepository.cs
+++ GuiaVegana/Data/Repository/Interfaces/IBusinessRepository.cs
@@ -1,6 +1,27 @@
-﻿namespace GuiaVegana.Data.Repository.Interfaces
+﻿using GuiaVegana.Entities;
+
+namespace GuiaVegana.Data.Repository.Interfaces
 {
     public class IBusinessRepository
     {
+        // Métodos GET
+        IEnumerable<Business> GetAllBusinesses();
+        Business GetBusinessById(int id);
+        IEnumerable<Business> GetBusinessesByRatings(IEnumerable<Rating> ratings);
+        IEnumerable<Business> GetBusinessesByDeliveries(IEnumerable<DeliveryType> deliveries);
+        IEnumerable<Business> GetBusinessesByTypes(IEnumerable<BusinessType> businessTypes);
+        IEnumerable<Business> GetBusinessesByZones(IEnumerable<Zone> zones);
+        IEnumerable<Business> GetPlantBasedBusinesses();
+        IEnumerable<Business> GetGlutenFreeBusinesses();
+        IEnumerable<Business> GetBusinessesOpenAt(DateTime currentTime);
+
+        // Métodos POST
+        void AddBusiness(Business business);
+
+        // Métodos PUT
+        void UpdateBusiness(Business business);
+
+        // Métodos DELETE
+        void DeleteBusiness(int id);
     }
 }
