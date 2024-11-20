c14de82bc808454c645b8fb0de9138ec269bf928
Olivia <olivialuciano42@gmail.com>
2024-11-20 18:26:36 -0300

    Repository, interface y controller de informative resourde

--- GuiaVegana/Controllers/InformativeResourceController.cs
+++ GuiaVegana/Controllers/InformativeResourceController.cs
@@ -1,12 +1,70 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using GuiaVegana.Data.Repository.Interfaces;
+using GuiaVegana.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GuiaVegana.Controllers
 {
-    public class InformativeResourceController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InformativeResourceController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly IInformativeResourceRepository _repository;
+
+        public InformativeResourceController(IInformativeResourceRepository repository)
         {
-            return View();
+            _repository = repository;
+        }
+
+        // GET: api/InformativeResource
+        [HttpGet]
+        public ActionResult<IEnumerable<InformativeResourceDTO>> GetAll()
+        {
+            var resources = _repository.GetAll();
+            return Ok(resources);
+        }
+
+        // GET: api/InformativeResource/{id}
+        [HttpGet("{id}")]
+        public ActionResult<InformativeResourceDTO> GetById(int id)
+        {
+            var resource = _repository.GetById(id);
+            if (resource == null)
+                return NotFound();
+
+            return Ok(resource);
+        }
+
+        // GET: api/InformativeResource/type/{type}
+        [HttpGet("type/{type}")]
+        public ActionResult<IEnumerable<InformativeResourceDTO>> GetByType(ResourceType type)
+        {
+            var resources = _repository.GetByType(type);
+            return Ok(resources);
+        }
+
+        // POST: api/InformativeResource
+        [HttpPost]
+        public IActionResult Add([FromBody] InformativeResourceToCreateDTO resourceToCreate)
+        {
+            _repository.Add(resourceToCreate);
+            return CreatedAtAction(nameof(GetById), new { id = resourceToCreate }, resourceToCreate);
+        }
+
+        // PUT: api/InformativeResource/{id}
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody] InformativeResourceToCreateDTO resourceToUpdate)
+        {
+            _repository.Update(id, resourceToUpdate);
+            return NoContent();
+        }
+
+        // DELETE: api/InformativeResource/{id}
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _repository.Delete(id);
+            return NoContent();
         }
     }
 }
--- GuiaVegana/Data/Repository/Implementations/InformativeResourceRepository.cs
+++ GuiaVegana/Data/Repository/Implementations/InformativeResourceRepository.cs
@@ -1,16 +1,70 @@
 ﻿using AutoMapper;
 using GuiaVegana.Data.Repository.Interfaces;
+using GuiaVegana.Entities;
+using GuiaVegana.Models;
 
 namespace GuiaVegana.Data.Repository.Implementations
 {
     public class InformativeResourceRepository : IInformativeResourceRepository
     {
-        private GuiaVeganaContext _context;
+        private readonly GuiaVeganaContext _context;
         private readonly IMapper _mapper;
+
         public InformativeResourceRepository(GuiaVeganaContext context, IMapper mapper)
         {
             _context = context;
             _mapper = mapper;
+        }
+
+        // GET: Get all informative resources
+        public IEnumerable<InformativeResourceDTO> GetAll()
+        {
+            var resources = _context.InformativeResources.ToList();
+            return _mapper.Map<IEnumerable<InformativeResourceDTO>>(resources);
+        }
+
+        // GET: Get informative resource by ID
+        public InformativeResourceDTO GetById(int id)
+        {
+            var resource = _context.InformativeResources.FirstOrDefault(r => r.Id == id);
+            return _mapper.Map<InformativeResourceDTO>(resource);
+        }
+
+        // GET: Get informative resources by type
+        public IEnumerable<InformativeResourceDTO> GetByType(ResourceType type) // Nuevo método
+        {
+            var resources = _context.InformativeResources.Where(r => r.Type == type).ToList();
+            return _mapper.Map<IEnumerable<InformativeResourceDTO>>(resources);
+        }
+
+        // POST: Add a new informative resource
+        public void Add(InformativeResourceToCreateDTO informativeResourceToCreate)
+        {
+            var resource = _mapper.Map<InformativeResource>(informativeResourceToCreate);
+            _context.InformativeResources.Add(resource);
+            _context.SaveChanges();
+        }
+
+        // PUT: Update an existing informative resource
+        public void Update(int id, InformativeResourceToCreateDTO informativeResourceToUpdate)
+        {
+            var existingResource = _context.InformativeResources.FirstOrDefault(r => r.Id == id);
+            if (existingResource != null)
+            {
+                _mapper.Map(informativeResourceToUpdate, existingResource);
+                _context.SaveChanges();
+            }
+        }
+
+        // DELETE: Remove an informative resource by ID
+        public void Delete(int id)
+        {
+            var resource = _context.InformativeResources.FirstOrDefault(r => r.Id == id);
+            if (resource != null)
+            {
+                _context.InformativeResources.Remove(resource);
+                _context.SaveChanges();
+            }
         }
     }
 }
